d79b74b4e26b028b9a901acc26758768c139d47c
Agata Filho <agata78@gmail.com>
2016-10-09 17:35:14 -0400

    Convert a “relative URL” to an absolute URL.

--- client.py
+++ client.py
@@ -1,37 +1,15 @@
 import requests
+import bs4
 from bs4 import BeautifulSoup
-
-prettify = BeautifulSoup.prettify
-
-def find_link(html_content):
-    """(str) -> bs4.element.Tag or None
-
-    Return the first link on the main body of the article that is
-    not within parenthesis or italicized.
-    Since Wikipedia uses a template we assume the not
-    italicized link will be in one of the `p` tag paragraphs."""
-    soup = BeautifulSoup(html_content)
-    paragraphs = soup.find_all('p')
-    for p in paragraphs:
-        string = ''
-        for element in p:
-            if type(element) == bs4.element.NavigableString:
-                string += element
-            elif type(element) == bs4.element.Tag and s.name == 'a':
-                if balanced_parenths(string):
-                    return element
-            else:
-                string += element.get_text()
-    return None
+from urlparse import urljoin
+from urlparse import urlparse
 
 def find_philosophy(url, visited_url=[], path_length=0):
     """(str) -> int
 
     Return path length or -1 if the `url` does not lead to 
     philosophy page."""
-    # track previous url to check for loops
-    # if no link on the page -1
-    # if see some page again in one search -1
+    print 'URL: ', url
     r = requests.get(url)
     html_content = r.text
     philosophy_url = 'https://en.wikipedia.org/wiki/Philosophy'
@@ -46,13 +24,38 @@ def find_philosophy(url, visited_url=[], path_length=0):
         # Check if the page contains link that meets the requirements.
         # Check it have we not seen the link yet. Repeted link would indicate
         # we will never get to philosophy page.
-        if link:
-            path_length += 1
-            visited_url.append(url)
-            # Recurse
-            find_philosophy(link, visited_url, path_length)
+        if link and link.attrs.has_key('href'):
+            new_url = urljoin(url, link['href'])
+            if not differrent_path(url, new_url):
+                return -1
+            else:
+                path_length += 1
+                visited_url.append(url)
+                # Recurse
+                find_philosophy(new_url, visited_url, path_length)
         else:
             return -1
+
+def find_link(html_content):
+    """(str) -> bs4.element.Tag or None
+
+    Return the first link on the main body of the article that is
+    not within parenthesis or italicized.
+    Since Wikipedia uses a template we assume the not
+    italicized link will be in one of the `p` tag paragraphs."""
+    soup = BeautifulSoup(html_content, "html.parser")
+    paragraphs = soup.find_all('p')
+    for p in paragraphs:
+        string = ''
+        for element in p:
+            if type(element) == bs4.element.NavigableString:
+                string += element
+            elif type(element) == bs4.element.Tag and element.name == 'a':
+                if balanced_parenths(string):
+                    return element
+            else:
+                string += element.get_text()
+    return None
 
 def find_percentage(m):
     """(int, int) -> int
@@ -97,6 +100,12 @@ def balanced_parenths(string):
             balanced -= 1
     return balanced == 0
 
+def differrent_path(old_url, new_url):
+    """Make sure we are not staing on the same page."""
+    parsed_old = urlparse(old_url)
+    parsed_new = urlparse(new_url)
+    return parsed_old.path != parsed_new.path
+
 if __name__ == '__main__':
     url = 'https://en.wikipedia.org/wiki/New_York_City'
-    pass
+    print find_philosophy(url)
