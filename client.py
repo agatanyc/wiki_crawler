5797c33ea30b17e278919e09858a8e4d7170da27
Agata Filho <agata78@gmail.com>
2016-10-09 11:23:36 -0400

    Create distriubtion of pahh lengths for `m` pages.

--- client.py
+++ client.py
@@ -1,16 +1,15 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
-from stack import Stack
 
 prettify = BeautifulSoup.prettify
 
-def find_link(url):
-    """(str) -> str
+def find_link(html_content):
+    """(str) -> bs4.element.Tag or None
+
     Return the first link on the main body of the article that is
     not within parenthesis or italicized.
     Since Wikipedia uses a template we assume the not
     italicized link will be in one of the `p` tag paragraphs."""
-    html_content = urllib2.urlopen(url)
     soup = BeautifulSoup(html_content)
     paragraphs = soup.find_all('p')
     for p in paragraphs:
@@ -19,13 +18,73 @@ def find_link(url):
             if type(element) == bs4.element.NavigableString:
                 string += element
             elif type(element) == bs4.element.Tag and s.name == 'a':
-                if matched_parenths(string):
+                if balanced_parenths(string):
                     return element
             else:
                 string += element.get_text()
     return None
 
-def matched_parenths(string):
+def find_philosophy(url, visited_url=[], path_length=0):
+    """(str) -> int
+
+    Return path length or -1 if the `url` does not lead to 
+    philosophy page."""
+    # track previous url to check for loops
+    # if no link on the page -1
+    # if see some page again in one search -1
+    r = requests.get(url)
+    html_content = r.text
+    philosophy_url = 'https://en.wikipedia.org/wiki/Philosophy'
+    # Number of requests it took to get to philosophy page.
+    path_length = 0
+    if url == philosophy_url:
+        return path_length
+    elif url in visited_url:
+        return -1
+    else:
+        link = find_link(html_content)
+        # Check if the page contains link that meets the requirements.
+        # Check it have we not seen the link yet. Repeted link would indicate
+        # we will never get to philosophy page.
+        if link:
+            path_length += 1
+            visited_url.append(url)
+            # Recurse
+            find_philosophy(link, visited_url, path_length)
+        else:
+            return -1
+
+def find_percentage(m):
+    """(int, int) -> int
+
+    Return the percentage of pages that lead to philosophy from 
+    `m` number of pages total.
+    """
+    # n is the number of pages that lead to philosophy
+    # m is the number of pages we are considering in the search
+    n = 0
+    for i in range(m):
+        # find random page
+        url = 'https://en.wikipedia.org/wiki/Special:Random'
+        found = []
+        if find_philosophy(url) != -1:
+            n += 1
+            found.append(url)
+    percentage = n // m * 100
+    return percentage, found
+
+def distribution(m):
+    """() -> dict
+
+    Distrbution of path lengths for m pages.
+    """
+    distr = {}
+    found = find_percentage(m)[1]
+    for url in found:
+        distr[url] = find_philosophy(url)
+    return distr
+
+def balanced_parenths(string):
     """(str) -> bool
 
     Return True if `string` contains the same number of opening and closing parenths, 
@@ -38,69 +97,6 @@ def matched_parenths(string):
             balanced -= 1
     return balanced == 0
 
-def find_philosophy(url, visited_url=[], path_length=0):
-    """(str) -> int
-
-    Return path length or -1 if the `url` does not lead to 
-    philosophy page."""
-    # track previous url to check for loops
-    # if no link on the page -1
-    # if see some page again in one search -1
-    philosophy_url = 'https://en.wikipedia.org/wiki/Philosophy'
-    path_length = 0
-    if url == philosophy_url:
-        return path_length
-    else:
-        link = find_link(url)
-        # Check if the page contains link that meets the requirements.
-        # Check it have we not seen the link yet. Repeted link would indicate
-        # we will never get to philosophy page
-        if link and link url not in visited_url:
-            path_length += 1
-            visited_url.append(url)
-            # Recurse
-            find_philosophy(link, visited_url, path_length)
-        else:
-            return -1
-
-def find_path_length(url):
-    """(str) -> int
-    Check how many requests to took to get to
-    philosophy page."""
-    if find_philosophy(url):
-        pass
-
-
-def find_percentage(n, m):
-    """(int) -> int
-    Return the percentage of pages often lead to philosophy.
-    """
-    # n is the number of pages that lead to philosophy
-    # m is the number of pages we are considering in the search
-    percentage = n // m * 100
-
-def distribution(list):
-    distribution = {}
-    for url in list:
-        dist[url] = find_path_length(url)
-    return distribution
-
-# helper functions
-
-def get_soup(url):
-    html_content = urllib2.urlopen(url)
-    return BeautifulSoup(html_content)
-
 if __name__ == '__main__':
-    url = 'https://en.wikipedia.org/wiki/Uniform_Resource_Locator'
-    s1 = '((()))'
-    s2 = '()()'
-    s3 = '(()'
-
-    #find_link(url)
-    print matched_parenths(s1)
-    print matched_parenths(s2)
-    print matched_parenths(s3)
-
-
-
+    url = 'https://en.wikipedia.org/wiki/New_York_City'
+    pass
