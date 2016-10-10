9d00860454403ee64782fbd16af8c40fa3e67b4e
Agata Filho <agata78@gmail.com>
2016-10-10 10:32:26 -0400

    WIP

--- client.py
+++ client.py
@@ -3,37 +3,42 @@ import bs4
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 from urlparse import urlparse
+from time import sleep
 
 def find_philosophy(url, visited_url=[], path_length=0):
     """(str) -> int
 
     Return path length or -1 if the `url` does not lead to 
     philosophy page."""
-    print 'URL: ', url
     r = requests.get(url)
+    print 'URL: ', r.url, 'PATH LENGTH: ', path_length
+
     html_content = r.text
     philosophy_url = 'https://en.wikipedia.org/wiki/Philosophy'
-    # Number of requests it took to get to philosophy page.
-    path_length = 0
-    if url == philosophy_url:
+    if r.url == philosophy_url:
+        print 'Reached Philosophy'
+        # Number of requests it took to get to philosophy page.
         return path_length
-    elif url in visited_url:
+    # Check it have we not seen the link yet. Repeted link would indicate
+    # we will never get to philosophy page.
+    elif r.url in visited_url:
+        print 'this is a loop'
         return -1
     else:
         link = find_link(html_content)
         # Check if the page contains link that meets the requirements.
-        # Check it have we not seen the link yet. Repeted link would indicate
-        # we will never get to philosophy page.
         if link and link.attrs.has_key('href'):
-            new_url = urljoin(url, link['href'])
-            if not differrent_path(url, new_url):
+            new_url = urljoin(r.url, link.attrs['href'])
+            if not differrent_path(r.url, new_url):
+                print 'This is a same page', new_url
                 return -1
             else:
                 path_length += 1
-                visited_url.append(url)
+                visited_url.append(r.url)
                 # Recurse
-                find_philosophy(new_url, visited_url, path_length)
+                return find_philosophy(new_url, visited_url, path_length)
         else:
+            print 'This is not a link', link
             return -1
 
 def find_link(html_content):
@@ -57,35 +62,49 @@ def find_link(html_content):
                 string += element.get_text()
     return None
 
-def find_percentage(m):
-    """(int, int) -> int
+def find_percentage(urls):
+    """(list) -> int
 
-    Return the percentage of pages that lead to philosophy from 
-    `m` number of pages total.
+    Return the percentage of pages that lead to philosophy.
     """
     # n is the number of pages that lead to philosophy
-    # m is the number of pages we are considering in the search
     n = 0
-    for i in range(m):
-        # find random page
-        url = 'https://en.wikipedia.org/wiki/Special:Random'
-        found = []
-        if find_philosophy(url) != -1:
+    for url in urls:
+        if find_philosophy(url, [], 0) != -1:
             n += 1
-            found.append(url)
-    percentage = n // m * 100
-    return percentage, found
+    percentage = n * 100 / len(urls)
+    return percentage
 
-def distribution(m):
-    """() -> dict
+def random_percentage(m):
+    pages = []
+    for i in range(m):
+        p = 'https://en.wikipedia.org/wiki/Special:Random'
+        pages.append(p)
+    return find_percentage(pages)
 
-    Distrbution of path lengths for m pages.
+
+def distribution(urls):
+    """(list) -> list
+
+    Distrbution of path lengths to reach `philosophy` page.
+    Elements of `urls` are the starting url.
     """
-    distr = {}
-    found = find_percentage(m)[1]
-    for url in found:
-        distr[url] = find_philosophy(url)
+    distr = []
+    for url in urls:
+        r = find_philosophy(url, [], 0)
+        if r != -1:
+            distr.append(r)
     return distr
+
+def random_distribution(m):
+    """(int) -> list
+
+    Distribution of path lengths for `m` pages."""
+    urls = []
+    for i in range(m):
+        p = 'https://en.wikipedia.org/wiki/Special:Random'
+        urls.append(p)
+    return distribution(urls)
 
 def balanced_parenths(string):
     """(str) -> bool
@@ -101,11 +120,27 @@ def balanced_parenths(string):
     return balanced == 0
 
 def differrent_path(old_url, new_url):
-    """Make sure we are not staing on the same page."""
+    """(str, str) -> bool
+
+    Make sure we are not staying on the same page."""
     parsed_old = urlparse(old_url)
     parsed_new = urlparse(new_url)
     return parsed_old.path != parsed_new.path
 
 if __name__ == '__main__':
-    url = 'https://en.wikipedia.org/wiki/New_York_City'
-    print find_philosophy(url)
+    url = 'https://en.wikipedia.org/wiki/New_York_City'  # a loop
+    url2 = 'http://en.wikipedia.org/wiki/Art'
+    #print find_philosophy(url)
+    #print ''
+    #print find_philosophy(url2)
+    #urls = ['https://en.wikipedia.org/wiki/David_Marr_(neuroscientist)',
+    #        'https://en.wikipedia.org/wiki/Reinevatn',
+    #        'https://en.wikipedia.org/wiki/Alpine_speedwell']
+    #for url in urls:
+    #    print "Testing url: ", url
+    #    result = find_philosophy(url)
+    #    print "*** Result: ", result
+    #print ''
+    print 'PERCENTAGE', find_percentage(3)
+    print ''
+    print 'DISTRIBUTIION', distribution(3)
